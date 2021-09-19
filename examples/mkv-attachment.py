4aedb8693daabd9ea53c4904ba564080e1effd85
Marcel Moreaux <git@moreaux.nl>
2021-09-19 17:46:26 +0200

    Added logging, duration info to examples/mkv-attachment

--- examples/mkv-attachment.py
+++ examples/mkv-attachment.py
@@ -3,9 +3,14 @@
 import sys
 import enzyme
 import shutil
+import logging
+
+#logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 with open(sys.argv[1], 'rb') as fd:
 	mkv = enzyme.MKV(fd)
+
+print(mkv.info)
 
 for a in mkv.attachments:
 	print(a)
