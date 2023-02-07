15f7ff3e3b5691a87bb3582488240bd6b3b441d3
Marcel Moreaux <git@moreaux.nl>
2023-02-07 21:17:27 +0100

    Added do-tagged support to serf

--- serf.py
+++ serf.py
@@ -7,31 +7,70 @@
 
 import os
 import sys
+import subprocess
 
 import dbs
 
-if len(sys.argv) < 2 or sys.argv[1] not in {'find-tagged', 'mark-seen'}:
+if len(sys.argv) < 2 or sys.argv[1] not in {'find-tagged', 'mark-seen', 'do-tagged'}:
 	print(f'Usage:')
 	print(f'  {sys.argv[0]} find-tagged          Recursively lists all tagged files.')
+	print(f'  {sys.argv[0]} do-tagged <cmd> <args> \'*\' <args>')
 	print(f'  {sys.argv[0]} mark-seen <file(s)>  Mark files as seen.')
 	exit(1)
+
 
 def escape(path):
 	return '"' + path.replace('\\', '\\\\').replace('"', '\\"') + '"'
 
-def process(path):
+
+def find_tagged(path):
 	index = dbs.json_read([path, dbs.INDEX_DB_NAME], dbs.INDEX_DB_SCHEMA)
 	state = dbs.json_read([path, dbs.STATE_DB_NAME], dbs.STATE_DB_SCHEMA)
 
+	files = []
 	for item in index['files']:
 		if state[item['name']].get('tagged', False):
 			if item['isdir']:
-				process(os.path.join(path, item['name']))
+				files += find_tagged(os.path.join(path, item['name']))
 			else:
-				print(escape(os.path.join(path, item['name'])))
+				files.append(os.path.join(path, item['name']))
+	return files
+
 
 if sys.argv[1] == 'find-tagged':
-	process('')
+	for fn in find_tagged(''):
+		print(fn)
+
+if sys.argv[1] == 'do-tagged':
+	pre_args, post_args = [], []
+	current = pre_args
+	mode = []
+	for arg in sys.argv[2:]:
+		if arg in {'*', '?'}:
+			mode.append(arg)
+			current = post_args
+		else:
+			current.append(arg)
+
+	if len(mode) != 1:
+		print('Exactly one argument should be "*" or "?"')
+		exit(1)
+	mode = mode[0]
+
+	if len(pre_args) < 1:
+		print(f'Need a command before {mode}')
+		exit(1)
+
+	files = find_tagged('')
+	if not files:
+		print('No tagged files.')
+		exit(1)
+
+	if mode == '?':
+		for fn in files:
+			subprocess.run(pre_args + [fn] + post_args)
+	if mode == '*':
+		subprocess.run(pre_args + files + post_args)
 
 if sys.argv[1] == 'mark-seen':
 	count = 0
